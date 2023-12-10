af5c6236f142888358588b89f9a5926c1f1c073d
Emmanuel Trinidad <32442963+emmtrinidad@users.noreply.github.com>
2023-12-09 18:23:09 -0700

    added view for checking downloads of certain user - will need to add admin permissions at the end

---
+++ project/adminView.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from app import db
+from models import User, Download, Schedule
+from flask_login import login_user, login_required, logout_user, current_user
+
+#TODO perms
+
+
+adminView = Blueprint('adminView', __name__)
+
+@adminView.route('/admin')
+def admin():
+
+    return render_template('admin.html')
+
+@adminView.route('/admin/users')
+def adminUsers():
+
+    users = User.query.order_by(User.id).all()
+
+    return render_template('adminusers.html', users = users)
+
+@adminView.route('/admin/<int:userId>/downloads')
+def adminViewDownloads(userId):
+
+    user = User.query.filter_by(id = userId).first()
+
+    downloads = Download.query.filter_by(userId = user.id).all()
+    schedules = []
+
+    for x in downloads:
+        schedule = Schedule.query.filter_by(scheduleId = x.scheduleId).first() 
+
+        if schedule:
+            schedules.append(schedule)
+
+    return render_template('adminuserdownloads.html', user = user, schedules = schedules)
