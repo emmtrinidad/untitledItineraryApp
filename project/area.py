921572ef9eff5b155c4b1fe2bc75870bd43c069c
Emmanuel Trinidad <32442963+emmtrinidad@users.noreply.github.com>
2023-12-10 17:19:34 -0700

    created search bar for locations

--- project/area.py
+++ project/area.py
@@ -8,14 +8,14 @@ area = Blueprint('area', __name__)
 
 @area.route('/locations')
 def locations():
-    return 'placeholder for locations'
+    return render_template('area/locations.html')
 
 @area.route('/locations/countries')
 def countries():
 
     countries = Country.query.order_by(Country.countryName).all()
 
-    return render_template('countries.html', countries = countries)
+    return render_template('area/countries.html', countries = countries)
 
 @area.route('/locations/countries/<string:countryName>')
 def country(countryName):
@@ -23,49 +23,65 @@ def country(countryName):
     country = countryQueryData(countryName)
 
     if not country:
-        return 'no countries but it works'
+        return 'no countries but it works'  
     
-    return 'works'
+    return render_template('area/country.html', country = country)
 
 @area.route("/locations/<string:countryName>/provinces")
 def provinces(countryName):
     countryQueryData(countryName)
 
-    provinces = Province.query.order_by(Province.provinceCode).all()
-    return render_template("provinces.html", provinces=provinces)
+    provinces = Province.query.order_by(Province.provinceName).all()
+    return render_template("area/provinces.html", provinces=provinces)
 
-@area.route("/locations/<string:countryName>/provinces/<string:provinceCode>")
-def province(countryName, provinceCode):
+@area.route("/locations/<string:countryName>/provinces/<string:provinceName>")
+def province(countryName, provinceName):
     countryQueryData(countryName)
     
-    province = provinceQueryData(provinceCode)
+    province = provinceQueryData(provinceName)
 
     if not province:
-        return "province does not exist in database"
+        flash('province does not exist')
+        return redirect(url_for('area.provinces(countryName)'))
     
-    return render_template("province.html", province=province)
+    return render_template("area/province.html", province=province)
 
-@area.route("/locations/<string:countryName>/<string:provinceCode>/cities")
-def cities(countryName, provinceCode):
+@area.route("/locations/<string:countryName>/<string:provinceName>/cities")
+def cities(countryName, provinceName):
     countryQueryData(countryName)
-    provinceQueryData(provinceCode)
+    provinceQueryData(provinceName)
 
     cities = City.query.order_by(City.name).all()
-    return render_template("cities.html", cities=cities)
+    return render_template("area/cities.html", cities=cities)
 
-@area.route("/locations/<string:countryName>/<string:provinceCode>/cities/<string:name>")
-def city(countryName, provinceCode, name):
+@area.route("/locations/<string:countryName>/<string:provinceName>/cities/<string:cityCode>")
+def city(countryName, provinceName, cityCode):
     countryQueryData(countryName)
-    provinceQueryData(provinceCode)
-    city = cityQueryData(name)
-    return render_template("city.html", city=city)
+    provinceQueryData(provinceName)
+    city = cityQueryData(cityCode)
+    return render_template("area/city.html", city=city)
+
+@area.route("/search", methods = ['POST'])
+def search():
+
+    text = request.form.get('search')
+
+    if not text:
+        flash('please enter text')
+        redirect(url_for('area.countries'))
+
+    possibleCities = City.query.filter(City.name.contains(text)).all()
+    possibleProvinces = Province.query.filter(Province.provinceName.contains(text)).all()
+    possibleCountries = Country.query.filter(Country.countryName.contains(text)).all()
+
+    return render_template('area/searchresults.html', countries = possibleCountries, provinces = possibleProvinces, cities = possibleCities)
 
 # Functions for retrieving data
 def countryQueryData(countryName):
     return Country.query.filter_by(countryName = countryName).first()
 
-def provinceQueryData(provinceCode):
-    return Province.query.filter_by(provinceCode=provinceCode).first()
+def provinceQueryData(provinceName):
+    return Province.query.filter_by(provinceName=provinceName).first()
 
-def cityQueryData(cityName):
-    return City.query.filter_by(name=cityName).first()
+def cityQueryData(cityCode):
+    return City.query.filter_by(airportCode = cityCode).first()
--- project/models.py
+++ project/models.py
@@ -73,7 +73,7 @@ class Country(db.Model):
     cities = db.relationship('City', backref = 'country', lazy = True)
 
 class Province(db.Model):
-    provinceCode = db.Column(db.Integer, primary_key = True)
+    provinceName = db.Column(db.String(200), primary_key = True)
     countryName = db.Column(db.String(200), db.ForeignKey('country.countryName'), nullable = False)
     timezone = db.Column(db.String(3), nullable = False)
     climate = db.Column(db.String(200), nullable = False)
@@ -82,8 +82,8 @@ class Province(db.Model):
 class City(db.Model):
     airportCode = db.Column(db.String(3), primary_key = True)
     name = db.Column(db.String(200), nullable = False)
-    provinceCode = db.Column(db.Integer, db.ForeignKey('province.provinceCode'))
-    countryName = db.Column(db.Integer, db.ForeignKey('country.countryName'))
+    provinceName = db.Column(db.Integer, db.ForeignKey('province.provinceName'))
+    countryName = db.Column(db.String(200), db.ForeignKey('country.countryName'))
     attractions = db.relationship('Attraction', backref = 'attraction', lazy = True)
 
 class Review(db.Model):
